a70b8dc5477e749a03ae56e1e0171c672afa3797
wesualvisual <wesualvisual.com@gmail.com>
2025-05-30 23:05:40 +0800

    Upload

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2562-02-r5",
+      "name": "2562-02-R5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.02442150158830181,
+          "pitch": 0.3628024705452848,
+          "rotation": 0,
+          "target": "1-2562-01-r5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2562-01-r5",
+      "name": "2562-01-R5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.543094987617277,
+          "pitch": 0.5475852599518376,
+          "rotation": 11.780972450961727,
+          "target": "0-2562-02-r5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
